4622b0df4863d52da01991e2bc5f242a918683b7
a1824jj <a1824jj@aiit.ac.jp>
2020-04-16 12:09:19 +0900

    make project

---
+++ like_instagram_used_by_firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  like_instagram_used_by_firebase
+//
+//  Created by JONGWOO JIN on 2020/04/16.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
