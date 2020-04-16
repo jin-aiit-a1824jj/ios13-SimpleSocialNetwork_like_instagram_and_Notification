25c7c6d80f34f3eb468b3f86b4e5cfca3d5ebcf4
a1824jj <a1824jj@aiit.ac.jp>
2020-04-16 16:31:19 +0900

    create tableview + cell in feedVC

---
+++ like_instagram_used_by_firebase/FeedCell.swift
@@ -0,0 +1,33 @@
+//
+//  FeedCell.swift
+//  like_instagram_used_by_firebase
+//
+//  Created by JONGWOO JIN on 2020/04/16.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func likeButtonClicked(_ sender: Any) {
+        
+    }
+}
