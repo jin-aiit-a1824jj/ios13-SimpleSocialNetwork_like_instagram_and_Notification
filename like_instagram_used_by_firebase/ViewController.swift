344cc7fdedfb2cd8fbac7e7ad7fec7bb103d60b0
a1824jj <a1824jj@aiit.ac.jp>
2020-04-16 12:54:10 +0900

    create user

--- like_instagram_used_by_firebase/ViewController.swift
+++ like_instagram_used_by_firebase/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -24,6 +25,28 @@ class ViewController: UIViewController {
     
     @IBAction func signUpClicked(_ sender: Any) {
         
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail:emailText.text! , password: passwordText.text!) { (authdata, error) in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Unkwon error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+                
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
+        
+        
+    }
+    
+    func makeAlert(titleInput:String, messageInput:String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
